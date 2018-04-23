07f5b13d9a1d03fcf0985d33c9963e81720606fa
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-04-23 11:38:15 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Beyonce: "Crazy in Love"};
+
+function updatePlaylist(obj, key, value) {
+  obj.key = value;
+  return obj;
+}
+
+function removeFromPlaylist(obj, key) {
+  delete obj.key;
+  return obj;
+}

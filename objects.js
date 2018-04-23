316e4d783df6965ceabb8a72a7625b5a2c16775c
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-04-23 11:47:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {Beyonce: "Crazy in Love"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist.$artistName = songTitle;
   return playlist;
 }
 
